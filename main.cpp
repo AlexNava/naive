6d2bf9a63e27d898ca489b8e17bde62936312923
Alessandro Nava <alessandro.nava@txtgroup.com>
2023-08-11 16:21:41 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "renderer.hpp"
+
+int main(int argc, char *argv[])
+{
+    Renderer rend;
+
+    return 0;
+}
---
+++ renderer.hpp
@@ -0,0 +1,16 @@
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+
+class Renderer
+{
+private:
+    glm::dvec3 points[100];
+
+public:
+
+};
+
+#endif // RENDERER_HPP
