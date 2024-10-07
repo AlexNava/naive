8a1d716aed442cf5436c8740ea958ee91b63a6cd
Alex <alex.il.nava@gmail.com>
2024-10-07 09:11:21 +0200

    stub of rasterization code * threading code to be fixed

--- 3d/rasterizer.cpp
+++ 3d/rasterizer.cpp
@@ -1,8 +1,34 @@
 #include "rasterizer.hpp"
 #include "screen.hpp"
 
-Rasterizer::Rasterizer() {
+Rasterizer::Rasterizer()
+{
+    const int MAX_RASTER_THREADS = 4;
+
     m_subpixelEdges = false;
+
+    // create threads
+    m_threadPtrs.clear();
+    for (int i = 0; i < MAX_RASTER_THREADS; ++i)
+    {
+        WorkerData* tempWkData = new WorkerData();
+        tempWkData->pRasterizer = this;
+        tempWkData->firtLine = 0;
+        tempWkData->lastLine = 0;
+        tempWkData->pStartMutex = new std::mutex();
+        tempWkData->pStartMutex->lock();
+        tempWkData->pEndMutex = new std::mutex();
+        tempWkData->pEndMutex->lock();
+        tempWkData->pThread = new std::thread((void *)scanlineWorker, tempWkData);
+        m_threadPtrs.push_back(tempWkData);
+    }
+
+}
+
+Rasterizer::~Rasterizer()
+{
+    // join threads
+    stopAllWorkers();
 }
 
 void Rasterizer::setPTargetScreen(Screen *newPTargetScreen)
@@ -11,5 +37,63 @@ void Rasterizer::setPTargetScreen(Screen *newPTargetScreen)
     if (m_pTargetScreen && m_pTargetScreen->height() > leftEdge.size())
     {
         leftEdge.resize(m_pTargetScreen->height());
+    }
+}
+
+Screen *Rasterizer::pTargetScreen() const
+{
+    return m_pTargetScreen;
+}
+
+void Rasterizer::setPTexture(Texture *newPTexture)
+{
+    m_pTexture = newPTexture;
+}
+
+void Rasterizer::startDrawingScanlines()
+{
+    for (int i = 0; i < m_threadPtrs.size(); ++i)
+    {
+        m_threadPtrs.at(i)->pStartMutex->unlock();
+    }
+}
+
+void Rasterizer::waitScanlines()
+{
+    for (int i = 0; i < m_threadPtrs.size(); ++i)
+    {
+        m_threadPtrs.at(i)->pEndMutex->lock();
+    }
+}
+
+void Rasterizer::stopAllWorkers()
+{
+    for (int i = 0; i < m_threadPtrs.size(); ++i)
+    {
+        m_threadPtrs.at(i)->stopRequested = true;
+        m_threadPtrs.at(i)->pStartMutex->unlock();
+        m_threadPtrs.at(i)->pThread->join();
+    }
+}
+
+void scanlineWorker(void *pWkData)
+{
+    WorkerData *pData = (WorkerData*) pWkData;
+    Rasterizer *pRasterizer = pData->pRasterizer;
+    uint16_t scanline = 0;
+
+    while (true)
+    {
+        pData->pStartMutex->lock();
+
+        if (pData->stopRequested)
+            break;
+
+        for (scanline = pData->firtLine; scanline <= pData->lastLine; ++scanline)
+        {
+            // draw scanlines
+        }
+
+        pData->pEndMutex->unlock();
     }
 }
--- 3d/rasterizer.hpp
+++ 3d/rasterizer.hpp
@@ -1,10 +1,14 @@
 #ifndef RASTERIZER_HPP
 #define RASTERIZER_HPP
 
+#include <thread>
+#include <mutex>
 #include <vector>
 #include "naive_defs.hpp"
 
 class Screen;
+class Texture;
+class Rasterizer;
 
 struct ScanlineEnd
 {
@@ -15,22 +19,49 @@ struct ScanlineEnd
     uint8_t spare[3]; // alignment
 };
 
+struct WorkerData
+{
+    std::thread *pThread;
+    Rasterizer  *pRasterizer;
+    uint16_t     firtLine;
+    uint16_t     lastLine;
+    std::mutex  *pStartMutex;  // released by caller, locked by worker
+    std::mutex  *pEndMutex;    // released by worker, locked by caller
+    bool         stopRequested;
+};
+
 class Rasterizer
 {
 public:
     Rasterizer();
+    ~Rasterizer();
 
     void setPTargetScreen(Screen *newPTargetScreen);
 
     void renderTriangle();
 
+    Screen *pTargetScreen() const;
+    void setPTexture(Texture *newPTexture);
+
 private:
     Screen *m_pTargetScreen;
+    Texture *m_pTexture;
+    // @todo support texture units? fixed?
+    // @todo define blend operations in texture unit parameters?
+    // @todo make scren and texture classes related?
+
     std::vector<ScanlineEnd> leftEdge;
     std::vector<ScanlineEnd> rightEdge;
 
-
     bool m_subpixelEdges;
+    std::vector<WorkerData*> m_threadPtrs;
+
+    void startDrawingScanlines();
+    void waitScanlines();
+
+    void stopAllWorkers();
 };
+
+void scanlineWorker(void *);
 
 #endif // RASTERIZER_HPP
